15fc4f1be1404302b977ff8476bbd8d6bbac85a6
Sam Kerr <kerr.sam@gmail.com>
2012-09-07 16:39:12 -0400

    Worked on view and made the add new page more robust.

--- lists/view.js
+++ lists/view.js
@@ -2,32 +2,55 @@ function(head, req)
 {
 	// !json templates.mustache
 	// !json templates.individualentry
+	// !json templates.emptybody
 	// !json templates.navbar
 	// !json templates.scripts
 	// !json templates.header
 	// !code templates/mustache.js
+	provides('html', function() {
+	
+		var count = 0;
+		results = '<div class="container-fluid"><div class="row-fluid"><div class="span12">';
+	
+		while(row = getRow())
+		{
+			if(row == null || row.value == null)
+				continue;
+			
+			var data = {
+					'type' : row.value.type,
+					'color' : row.value.color,
+					'brand' : row.value.brand,
+					'link' : row.value.link,
+					'notes' : row.value.notes,
+					'fit' : row.value.fit,
+					'documentname' : row.value._id,
+					'attachmentname' : row.value.attachmentname
+			};
+			
+			
+			results += Mustache.render(templates.individualentry, data);
+			count++;
+		}
+		 
+		results += '</div></div></div>';
+	
+		var navbar_contents = {
+				"view": "active"
+		};
+		navbar = Mustache.render(templates.navbar, navbar_contents);
+			
+		var view = {
+				"navbar": navbar,
+				"header": templates.header,
+				"scripts": templates.scripts,
+				"body": results
+				  };
+		
+		var final_results = Mustache.render(templates.emptybody, view);
+		
+		
+	    send(final_results);
+	});
+};
 
-	var data = {
-		'type' : 'hello_type',
-		'color' : 'hello_color',
-		'brand' : 'hello_brand',
-		'link' : 'hello_link',
-		'notes' : 'hello_notes',
-		'fit' : 'hello_fit',
-		'documentname' : 'hello_documentname',
-		'attachmentname' : 'hello_attachmentname'
-	};
-
-	var count = 0;
-	results = '<div class="container-fluid"><div class="row-fluid"><div class="span12">';
-
-	while(row = getRow())
-	{
-		results += Mustache.render(templates.individualentry, data);
-		count++;
-	}
-	 
-	results += '/div></div></div>;'
-
-    send(results);
-}
--- views/sortbytime/map.js
+++ views/sortbytime/map.js
@@ -1,4 +1,58 @@
-function(doc) {
- if(doc.created_at)
- { emit(doc.created_at, doc._id); }
-}
+function(doc) 
+{
+	jsonConcat = function(o1, o2) {
+		 for (var key in o2) {
+		  o1[key] = o2[key];
+		 }
+		 return o1;
+		}
+	
+	finaldoc = {};
+	
+	if(doc.created_at)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'created_at': doc.created_at } ); 
+	}
+	
+	if(doc.type)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'type': doc.type } ); 
+	}
+
+	if(doc.color)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'color': doc.color } ); 
+	}
+	
+	if(doc.brand)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'brand': doc.brand } ); 
+	}
+	
+	if(doc.link)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'link': doc.link } ); 
+	}
+	
+	if(doc.notes)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'notes': doc.notes } ); 
+	}
+	
+	if(doc.fit)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'fit': doc.fit } ); 
+	}
+	
+	if(doc.documentname)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'documentname': doc.documentname } ); 
+	}
+	
+	if(doc.attachmentname)
+	{ 
+		finaldoc = jsonConcat( finaldoc, { 'attachmentname': doc.attachmentname } ); 
+	}
+	
+	emit(doc._id, finaldoc);
+};
